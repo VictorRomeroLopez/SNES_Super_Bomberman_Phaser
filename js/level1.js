49ce92388066544830c10b2a20f680b802881d13
Víctor Romero López <victor.romero@enti.cat>
2019-11-09 16:20:53 +0100

    Add Tilemap to Phaser

---
+++ js/level1.js
@@ -0,0 +1,54 @@
+var SuperBomberman = SuperBomberman || {}
+
+SuperBomberman.level1 = {
+    
+    init:function()
+    {
+        console.log("init")
+    },
+    
+    preload:function()
+    {
+        console.log("preload")
+        
+        //---region LOAD_TILESET_IMAGES---//
+        {
+        this.load.image('buildings', "assets/levels/Pace_town.png");
+        this.load.image('destroyables', "assets/levels/Pace_town_destroyable.png");
+        this.load.tilemap('level1','assets/Tiled/level1.json', null, Phaser.Tilemap.TILED_JSON);
+        }
+        //---end region---//
+        
+    },
+    
+    create:function()
+    {
+        console.log("create")
+        
+        //---region ADD_IMAGES_TO_TILEMAP---//
+        {
+        this.map = this.game.add.tilemap('level1')
+        
+        //agreguem els spritesheets al map
+        this.map.addTilesetImage('buildings')
+        this.map.addTilesetImage('destroyables')
+        
+        //creem les layers al map
+        this.exteriorWalls  = this.map.createLayer('Exterior_Walls')
+        this.interiorWalls  = this.map.createLayer('Interior_Walls')
+        this.floor          = this.map.createLayer('Floor')
+        
+        //Escalat de les diferents layers del map
+        this.exteriorWalls.scale    = new PIXI.Point(gameOptions.gameScale,gameOptions.gameScale);
+        this.interiorWalls.scale    = new PIXI.Point(gameOptions.gameScale,gameOptions.gameScale);
+        this.floor.scale            = new PIXI.Point(gameOptions.gameScale,gameOptions.gameScale);
+        }
+        //---end region---//
+        
+    },
+    
+    update:function()
+    {
+        console.log("update")
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
 var SuperBomberman = SuperBomberman || {};
 
+var gameScale = 4
+
 var gameOptions = {
-  //Add generic variables   
-    gameWidth:1000,
-    gameHeight:1000
+    //Add generic variables
+    gameScale:this.gameScale,
+    gameWidth:17*16*this.gameScale,
+    gameHeight:13*16*this.gameScale
 };
 
 SuperBomberman.game = new Phaser.Game(gameOptions.gameWidth, gameOptions.gameHeight, Phaser.AUTO, null, this, false, false);
 
+//Aqui posem les diferents escenes que volem tenir
 SuperBomberman.game.state.add('main', SuperBomberman.mainMenu);
-SuperBomberman.game.state.start('main');
+SuperBomberman.game.state.add('level1', SuperBomberman.level1);
+
+//Aquesta es la escena amb la que inicia el joc
+SuperBomberman.game.state.start('level1');

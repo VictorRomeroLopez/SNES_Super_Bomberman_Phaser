cb62ded0ab3916f78e7652bc874cc2c5f7a54adb
Andreu Margarit <andreu.margarit@enti.cat>
2019-11-20 14:42:11 +0100

    Aleix tonto powerup hotfix

--- js/powerUpPrefab.js
+++ js/powerUpPrefab.js
@@ -5,30 +5,32 @@ SuperBomberman.powerUp_prefab = function(_game, _x, _y, _type, _level)
     this.posx = (_x * 16);
     this.posy = (_y * 16);
     this.type = _type;
-    console.log("create")
+   
     if(this.type == 1)
     {
         Phaser.Sprite.call(this, _game, this.posx, this.posy, 'bombPU');
-        this.animations.add('bomb', [0,1], 7, true);
-        this.animations.play('bomb');
+        this.animations.add('bomb1', [0,8], 10, true);
+        this.animations.play('bomb1');
     }
     else if(this.type == 2)
     {
         Phaser.Sprite.call(this, _game, this.posx, this.posy, 'bombPU');
-        this.animations.add('bomb', [0,1], 7, true);
-        this.animations.play('bomb');
+        this.animations.add('bomb2', [0,1], 10, true);
+        this.animations.play('bomb2');
     }
     else 
     { 
         Phaser.Sprite.call(this, _game, this.posx, this.posy, 'bombPU');
-        this.animations.add('power', [2,10], 7, true);
+        this.animations.add('power', [2,10], 10, true);
         this.animations.play('power');
     }
 
 	this.anchor.setTo(.5);
     this.game = _game;
     this.level = _level;
+    
     _game.physics.arcade.enable(this);
+    _game.add.existing(this);
 };
 
 SuperBomberman.powerUp_prefab.prototype = Object.create(Phaser.Sprite.prototype);
@@ -37,10 +39,10 @@ SuperBomberman.powerUp_prefab.prototype.constructor = SuperBomberman.powerUp_pre
 
 SuperBomberman.powerUp_prefab.prototype.update = function()
 {    
-    console.log("updating")
+    console.log("updating");
 
     this.game.physics.arcade.overlap(this, this.level.player, this.pickUpPowerUp, null, this.level);  
-};
+}
 
 SuperBomberman.powerUp_prefab.prototype.pickUpPowerUp = function(powerUp, player)
 {
@@ -56,4 +58,4 @@ SuperBomberman.powerUp_prefab.prototype.pickUpPowerUp = function(powerUp, player
     {
         player.power++;
     }
-};
+}
